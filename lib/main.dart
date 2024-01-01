6a958d202085fcaa8cdf71c91cb9b709d8bbe34c
Yihun Alemayehu <yankure01@gmail.com>
2024-01-01 14:23:19 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:chapasdk/chapasdk.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
 }
@@ -16,7 +18,13 @@ class HomePage extends StatelessWidget {
         title: const Text('Pay now'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: ElevatedButton(
+          onPressed: (){
+            Chapa.paymentParameters(context: context, publicKey: publicKey, currency: currency, amount: amount, email: email, phone: phone, firstName: firstName, lastName: lastName, txRef: txRef, title: title, desc: desc, namedRouteFallBack: namedRouteFallBack)
+          }, 
+          child: const Text('Pay now'),),
+      ),
     );
   }
 }
